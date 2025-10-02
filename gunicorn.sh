adb14a9303c3198f929b2a4580fa6ace21a179fc
Abhisek Ransingh <abhisekransingh96@yahoo.com>
2025-10-03 01:48:52 +0530

    version 9

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,1 +1,23 @@
 #!/bin/ash
+source env/bin/activate
+cd /var/lib/jenkins/workspace/django-cicd-pipeline/jenkin_python 
+
+python manage.py makemigrations
+python manage.py migrate
+
+echo "migation completeed"
+
+sudo cp -rf gunicorn.socket /etc/systmed/system/
+sudo cp -rf gunicorn.service /etc/systmed/system/
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "gunicor has started"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
