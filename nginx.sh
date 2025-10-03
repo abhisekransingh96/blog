b3cd11cf3898345e0e938044f459005a377738af
Abhisek Ransingh <abhisekransingh96@yahoo.com>
2025-10-03 22:05:07 +0530

    version16

--- nginx.sh
+++ nginx.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sudo cp -rf app.conf /etc/nginx/conf.d
+sudo cp -rf app.conf /etc/nginx/sites-available/app
 chmod 710 /var/lib/jenkins/workspace/django-cicd-pipeline
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+
 sudo nginx -t
 
 sudo systemctl daemon-reload
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
-echo "nginx has started"
+echo "gunicor has started"
 
 sudo systemctl status nginx
