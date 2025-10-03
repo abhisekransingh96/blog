72ee07f798b82831beb1dc213fdace47cd4e9b44
Abhisek Ransingh <abhisekransingh96@yahoo.com>
2025-10-03 17:48:47 +0530

    version15

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cp -rf app.conf /etc/nginx/conf.d
 chmod 710 /var/lib/jenkins/workspace/django-cicd-pipeline
-
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl daemon-reload
